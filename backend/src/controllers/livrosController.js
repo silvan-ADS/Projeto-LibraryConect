fbc13ade9bd4cdeec9ea71e351ab00a30fdd5019
Silva <silvinhoguilherme2016@gmail.com>
2026-06-01 22:43:18 -0300

    Adiciona e Valida e feature atualizarLivros

--- backend/src/controllers/livrosController.js
+++ backend/src/controllers/livrosController.js
@@ -108,6 +108,125 @@ const adicionarLivro = async (req, res) => {
   });
 };
 
+const atualizarLivro = async (req, res) => {
+  const { id } = req.params;
+  const livroId = Number(id); 
+
+  // Validando o ID do livro
+  if (!id || Number.isNaN(livroId)) {
+    return res.status(400).json({
+      erro: "Atenção: ID do livro inválido.",
+    });
+  }
+
+  // Verificar se o livro existe antes de atualizar
+  const { data: livroExistente, error: erroBusca } = await supabase
+    .from("livros")
+    .select("id")
+    .eq("id", livroId)
+    .maybeSingle();
+
+  if (erroBusca) {
+    return res.status(500).json({
+      erro: erroBusca.message,
+    });
+  }
+  if (!livroExistente) {
+    return res.status(404).json({
+      erro: "Livro não encontrado.",
+    });
+  }
+
+  const { titulo, autor, genero, anoPublicacao, quantidade, ISBN } = req.body;
+  const camposAtualizados = {};
+
+  //Validar a montagem dos campos
+  if (titulo !== undefined) camposAtualizados.titulo = titulo;
+  if (autor !== undefined) camposAtualizados.autor = autor;
+  if (genero !== undefined) camposAtualizados.genero = genero;
+
+  //Validar a quantidade
+  if (quantidade !== undefined && quantidade !== null) {
+    const qtdNum = Number(quantidade);
+    if (isNaN(qtdNum) || qtdNum < 0) {
+      return res.status(400).json({
+        erro: "Atenção: A quantidade deve ser um número inteiro positivo!",
+      });
+    }
+    camposAtualizados.quantidade = qtdNum;
+}
+
+//Validar o ano de publicação
+if (anoPublicacao !== undefined && anoPublicacao !== null) {
+  const anoNum = Number(anoPublicacao);
+  const anoAtual = new Date().getFullYear();
+  if (isNaN(anoNum) || anoNum > anoAtual) {
+    return res.status(400).json({
+      erro: "Atenção: O ano de publicação não pode ser no futuro!",
+    });
+  }
+  camposAtualizados.anoPublicacao = anoNum;
+}
+
+//Validar e Verificar o ISBN
+if (ISBN !== undefined && ISBN !== null) {
+  const isbnLimpo = ISBN.replace(/-/g, "");
+
+  if (isbnLimpo.Length !== 10 && isbnLimpo.length !== 13) {
+    return res.status(400).json({
+      erro: "Atenção: O ISBN deve conter 10 ou 13 dígitos (sem hífens)!",
+    }); 
+  }
+
+// Verifica se outro livro (id diferente) já possui o mesmo ISBN
+  const { data: isbnExistente, error: erroIsbn } = await supabase
+    .from("livros")
+    .select("id")
+    .eq("ISBN", isbnLimpo)
+    .neq("id", livroId)
+    .maybeSingle();
+
+    if(erroIsbn) {
+      return res.status(500).json({
+        erro: erroIsbn.message,
+      });
+    }
+
+    if(isbnExistente) {
+      return res.status(400).json({
+        erro: "Atenção: Já existe um livro cadastrado com este ISBN!",
+      });
+    }
+  
+
+  camposAtualizados.ISBN = isbnLimpo;
+  }
+
+//Se nenhum campo válido foi evniado, evita fazer uma requisição desnecessário ao banco
+if (Object.keys(camposAtualizados).length === 0) {
+  return res.status(400).json({
+    erro: "Atenção: Nenhum campo válido para atualização foi fornecido.",
+  });
+ }
+
+//Executa a atualização no banco de dados
+const { data, error } = await supabase
+  .from("livros")
+  .update(camposAtualizados)
+  .eq("id", livroId)
+  .select();
+
+if (error) {
+  return res.status(500).json({
+    erro: error.message,
+  });  
+ }
+
+res.json({
+  mensagem: "Livro atualizado com sucesso!",
+});
+};
+
 const deletarLivro = async (req, res) => {
   const { id } = req.params;
   const livroId = Number(id);
@@ -155,5 +274,6 @@ const deletarLivro = async (req, res) => {
 module.exports = {
   adicionarLivro,
   listarLivros,
+  atualizarLivro,
   deletarLivro,
 };
--- backend/src/routes/livros.routes.js
+++ backend/src/routes/livros.routes.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const {
   listarLivros,
   adicionarLivro,
+  atualizarLivro,
   deletarLivro,
 } = require("../controllers/livrosController");
 
 router.post("/", adicionarLivro);
 router.get("/", listarLivros);
+router.put("/:id", atualizarLivro);
 router.delete("/:id", deletarLivro);
 
 module.exports = router;
