5c2b50c832688a1d22643afec325023f07ab9f2e
José Lucas <github132026@gmail.com>
2026-06-08 19:48:35 -0300

    logo e botão logout

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,29 +1,80 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+import { FiLogOut } from "react-icons/fi";
+import { FaUsers, FaBook, FaExchangeAlt } from "react-icons/fa";
+import Logo from "../assets/libraryAzul-conectBranco.png"
+import "../styles/sidebar.css"
 
 function Sidebar() {
+  const navigate = useNavigate();
+
+  function logout() {
+    localStorage.removeItem("logado");
+    localStorage.removeItem("usuario");
+
+    navigate("/");
+  }
+
   return (
     <div
-      className="bg-primary text-white p-4"
+      className="bg-primary text-white p-4 d-flex flex-column sidebar"
       style={{
         width: "240px",
         minHeight: "100vh",
       }}
     >
-      <h3 className="mb-5">LibraryConnect</h3>
-
-      <div className="d-flex flex-column gap-3">
-        <Link className="text-white text-decoration-none" to="/usuarios">
-          Usuários
-        </Link>
-
-        <Link className="text-white text-decoration-none" to="/livros">
-          Livros
-        </Link>
-
-        <Link className="text-white text-decoration-none" to="/emprestimos">
-          Empréstimos
-        </Link>
+      <div className="mb-5 text-center cont-logo">
+        <img
+          src={Logo}
+          alt="Library Connect"
+          className="img-fluid"
+          style={{
+            maxWidth: "200px",
+          }}
+        />
       </div>
+
+      <div className="d-flex flex-column gap-2">
+        <NavLink
+          to="/usuarios"
+          className={({ isActive }) =>
+            `text-decoration-none p-3 rounded ${
+              isActive ? "bg-light text-primary fw-bold" : "text-white"
+            }`
+          }
+        >
+          <FaUsers className="me-2" /> Usuários
+        </NavLink>
+        <NavLink
+          to="/livros"
+          className={({ isActive }) =>
+            `text-decoration-none p-3 rounded ${
+              isActive ? "bg-light text-primary fw-bold" : "text-white"
+            }`
+          }
+        >
+          <FaBook className="me-2" /> Livros
+        </NavLink>
+        <NavLink
+          to="/emprestimos"
+          className={({ isActive }) =>
+            `text-decoration-none p-3 rounded ${
+              isActive ? "bg-light text-primary fw-bold" : "text-white"
+            }`
+          }
+        >
+          <FaExchangeAlt className="me-2"/> Empréstimos
+        </NavLink>
+      </div>
+
+      <button
+        className="btn btn-lg btn-light d-flex align-items-center gap-2 mt-auto p-3"
+        onClick={logout}
+      >
+        <FiLogOut />
+        Sair
+      </button>
     </div>
   );
 }
